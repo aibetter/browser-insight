9882aa0e4c1281ee1fcf31a67341556f45c039f2
litingyes <luz.liting@gmail.com>
2025-07-12 12:23:24 +0800

    feat: add tailwindcss

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
   modules: ['@wxt-dev/module-vue'],
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
   manifest: {
     name: 'Insight',
   },
