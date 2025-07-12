7159afd303af0facacb729b8df3a2c622bfbf6ad
litingyes <luz.liting@gmail.com>
2025-07-12 12:09:00 +0800

    ci(git-hook): add format

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
   modules: ['@wxt-dev/module-vue'],
   manifest: {
-    name:'Insight'
-  }
-});
+    name: 'Insight',
+  },
+})
